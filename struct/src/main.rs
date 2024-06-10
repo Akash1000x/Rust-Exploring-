09ec0955f078be89c67ce02bf5cb627d075876da
Akash kumar kumawat <kumawatap63@gmail.com>
2024-06-10 22:55:25 +0530

    struct done

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,3 +1,68 @@
+// struct User {
+//     name: String,
+//     user_name: String,
+//     age: u32,
+//     active: bool,
+//     number: u32,
+//     email: String,
+// }
+
+// fn main() {
+//     let user: User = User {
+//         name: String::from("Akash"),
+//         user_name: String::from("ak123"),
+//         age: 22,
+//         active: true,
+//         number: 145345,
+//         email: String::from("akash**@gmail.com"),
+//     };
+
+//     println!("Name: {}", user.name);
+//     println!("User Name: {}", user.user_name);
+//     println!("Age: {}", user.age);
+//     println!("Active: {}", user.active);
+//     println!("Number: {}", user.number);
+//     println!("Email: {}", user.email);
+// }
+
+// -----------traits-----------------------------------------
+
+// #[derive(Clone, Copy)]
+// struct User {
+//     age: u32,
+//     number: u32,
+// }
+
+// fn main() {
+//     let user1 = User {
+//         age: 22,
+//         number: 145345,
+//     };
+
+//     print_user(user1);
+//     println!("Age: {}", user1.number);
+// }
+
+// fn print_user(user: User) {
+//     println!("Age: {}", user.age);
+// }
+
+// ---------------------------Implementing structs--------------------------
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rect {
+        width: 30,
+        height: 50,
+    };
+    print!("The area of the rectangle is {}", rect.area());
 }
